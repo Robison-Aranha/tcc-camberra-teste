0d24b4132ff332a181ed060b3b86ba2628ce528a
Roger Almeida <roger.almeida@cwi.com.br>
2022-08-26 22:55:20 -0300

    :)

---
+++ api/src/main/java/cwi/br/com/TesteWebSocket/TesteWebSocketApplication.java
@@ -0,0 +1,18 @@
+package cwi.br.com.TesteWebSocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class TesteWebSocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteWebSocketApplication.class, args);
+	}
+
+
+
+}
---
+++ api/src/main/java/cwi/br/com/TesteWebSocket/controller/model/Status.java
@@ -0,0 +1,7 @@
+package cwi.br.com.TesteWebSocket.controller.model;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
